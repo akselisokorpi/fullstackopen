3a49509e292687cf74d480a9d1458d3ed962f201
Akseli Isokorpi <akseli.isokorpi@tuni.fi>
2024-12-13 22:41:50 +0200

    Added feedback buttons and counting

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -1,5 +1,25 @@
 import { useState } from 'react'
 
+const Display = ( props ) => {
+  const good = props.good;
+  const bad = props.bad;
+  const neutral = props.neutral;
+
+  return (
+    <div>
+      <li>good {good}</li>
+      <li>bad {bad}</li>
+      <li>neutral {neutral}</li>
+    </div>
+  );
+};
+
+const Button = ( props ) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -8,7 +28,12 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="Good" />
+      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />  
+      <h1>statistics</h1>
+      <Display good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
