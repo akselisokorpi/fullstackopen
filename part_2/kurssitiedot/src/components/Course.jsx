e5dee781af1687141169fc22d64e64eaf349e325
Akseli Isokorpi <akseli.isokorpi@tuni.fi>
2024-12-16 22:20:43 +0200

    Refactored App and Course & removed Note component

--- part_2/kurssitiedot/src/components/Course.jsx
+++ part_2/kurssitiedot/src/components/Course.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 
 const Part = props => <div>{props.name} {props.exercises}</div>
 
-const Header = ( props ) => <h1>{props.course.name}</h1>
+const Header = ( props ) => <h2>{props.course.name}</h2>
+
+const Total = ( props ) => {
+    const parts = props.course.parts.map(course => course.exercises)
+
+    return (
+        <div>
+            <h3>Total of {parts.reduce( (s, p) => s + p )} exercises</h3>
+        </div>
+    )
+}
 
 const Content = ( props ) => {
     return (
         <div>
-            {props.parts.map(part => 
+            {props.course.parts.map(part => 
                 <Part key={part.id} name={part.name} exercises={part.exercises} />
             )}
         </div>
@@ -18,7 +28,8 @@ const Course = ( props ) => {
     return (
         <div>
             <Header course={props.course} />
-            <Content parts={props.course.parts} />
+            <Content course={props.course} />
+            <Total course={props.course} />
         </div>
     )
 }
